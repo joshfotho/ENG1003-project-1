e8316b1cd7ab2d41891aea51cdf42586e0155d67
Josh Fotheringham <joshfotho@gmail.com>
2019-04-03 07:27:04 +0000

    added some basic code with an array

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main(){
-    
-    return 0;
+    int k = 1, counter = 0; //key that values are shifted by
+    int letters[26]; //array that stores letters
+    //set each element of the array to ASCII value for its respective letter
+    int counter; 
+    for(counter = 0; counter < 26; counter++) {
+        x[counter] = counter + 65; 
+        }
+    printf("%c", x[0]);
 }
