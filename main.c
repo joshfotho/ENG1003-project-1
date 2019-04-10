d4cd25da1cc90372ad3a91b784c52225e399f872
Josh Fotheringham <joshfotho@gmail.com>
2019-04-10 04:30:58 +0000

    cipher code added

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
-    int k = 1, counter = 0; //key that values are shifted by
-    int letters[26]; //array that stores letters
-    //set each element of the array to ASCII value for its respective letter
-    int counter; 
-    for(counter = 0; counter < 26; counter++) {
-        x[counter] = counter + 65; 
+    int length;
+    int counter = 0; // used in the for loop
+    char text[100]; // the text to be encrypted;
+    int k = 2;//the amount that that letters are shifted by
+    length = strlen(text);
+    FILE *input;
+    input = fopen("test.txt", "r");
+    if(input == NULL){
+        perror("fopen()");
+         }
+//    for (int i = 0; i < 4; i++) {
+//        fscanf(input, "%c", &text[i]);
+//        printf("read from file: %c\n", text[i]);
+//    }       
+
+   
+    //This for loop shifts each element of the string by 'k' letters
+    for(counter = 0; counter < length; counter++) {
+        (text[counter]) = ((text[counter] -65 + k + 26)%26) +65;  
         }
-    printf("%c", x[0]);
+    counter = 0;
+    printf("The encrypted message is: %s \n", text); 
+    
+    
+    
 }
