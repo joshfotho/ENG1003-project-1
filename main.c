a75cebd90b69f9ad050b0f6abee0b63ca180c138
joshfotho <48045513+joshfotho@users.noreply.github.com>
2019-04-13 14:59:00 +1000

    Update main.c
    added support for lower case letters. Cleaned up the formatting a bit and added some new comments.

--- main.c
+++ main.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    int counter = 0; // used in the for loop
+int main()
+{
+    int index = 0; // the position of each element int the array
     char text[100]; // the text to be encrypted;
     
-    int k = 2;//the amount that that letters are shifted by
+    int k = 3;//the amount that that letters are shifted by
     FILE *input;
     input = fopen("test.txt", "r");
-    if(input == NULL){
-        perror("fopen()");
-         }
-//    for (int i = 0; i < 4; i++) {
-//        fscanf(input, "%c", &text[i]);
-//        printf("read from file: %c\n", text[i]);
-//    }       
+    if(input == NULL)
+    {
+        perror("fopen()"); //prints a message if problem with file
+    }  
     
+    /*This for loop shifts each element of the string by 'k' 
+    letters one letter at a time */
+    for(index = 0; index < 100; index++)
+    {
+        fscanf(input, "%c", &text[index]);
         
-    
-    
-    
-    //This for loop shifts each element of the string by 'k' letters
-    counter = 0;
-    for(counter = 0; counter < 10; counter++) {
-        fscanf(input, "%c", &text[counter]);
-        if(text[counter] >=65 && text[counter] <=90){
-            (text[counter]) = ((text[counter] -65 + k + 26)%26) +65;
+        //capital letters
+        if(text[index] >=65 && text[index] <=90)
+        {
+            (text[index]) = ((text[index] -65 + k + 26)%26) +65;
+        }
+        
+        //lower case letters. converted to capitals
+        else if(text[index] >= 97 && text[index] <=122)//lower case
+        {
+            (text[index]) = ((text[index] - 97 + k +26)%26) + 65;
         }
 
-        }
-
+    }
     
-    printf("The encrypted message is: %s \n", text); 
-//    
-//    
+    printf("The encrypted message is: %s \n", text);//prints adjusted string
     
 }
