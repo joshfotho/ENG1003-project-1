c0dc8274926ba4cd2291bc6cf921997c5f2fff80
joshfotho <48045513+joshfotho@users.noreply.github.com>
2019-04-29 15:41:38 +1000

    Update substitutionEncrypt.c

--- substitutionEncrypt.c
+++ substitutionEncrypt.c
@@ -13,25 +13,28 @@ int substitutionEncrypt()
         perror("fopen()"); //prints a message if problem with file
     } 
     
-    FILE *output;
+    //opens a file to write to
+    FILE *output; 
     output = fopen("output.txt", "w");
     
+    //writes each element of file to an array
     while(feof(input) != 1 )
     {
         fscanf(input, "%c", &text[index]);
         index++;
     }
-    int length = strlen(text);
+    int length = strlen(text); //finds length of array
     
-    index = 46;
+    index = 46; //elements of the file before 46 are information and we don't want to encrypt them
     
-    for(index > 46; index < length; index ++)
+    for(index > 46; index < length; index ++) 
     
     {
-        if(text[index] >= 65 && text[index]<= 90)
+        if(text[index] >= 65 && text[index]<= 90) //only want to encrypt letters
         {
-            printf("%c", text[20 + (text[index]-65)]);
-            fprintf(output, "%c", text[20 + (text[index]-65)]);
+            //swaps each element of the message to be encrypted with its corresponding letter in the key. Prints to terminal and output file
+            printf("%c", text[20 + (text[index]-65)]); 
+            fprintf(output, "%c", text[20 + (text[index]-65)]);  
         }   
         
     }
